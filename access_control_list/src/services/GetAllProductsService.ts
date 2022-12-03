3ba6a9adf99b94460fc4a05c17ceddd8cc35789a
fabiojunik <fabiojunik@gmail.com>
2022-12-03 16:45:36 +0100

    Revert "initial commit"
    This reverts commit b985dc18c47ad0bd2f6c4fafd2db7e6bcaeb73ff.

--- access_control_list/src/services/GetAllProductsService.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from "../entities/Product";
-import { ProductRepository } from "../repositories";
-
-export class GetAllProductsService {
-  async execute(): Promise<Product[]> {
-    const products = await ProductRepository().find();
-    return products;
-  }
-}
