0442bb7eb9efc2bd80dfb9a7a70df28c75c4d16b
fabiojunik <fabiojunik@gmail.com>
2023-11-03 14:19:27 +0100

    test: deve salvar editora no banco de dados

--- tests/editoraAPI/src/tests/models/editora.test.js
+++ tests/editoraAPI/src/tests/models/editora.test.js
@@ -1,4 +1,6 @@
-import { describe, expect } from '@jest/globals';
+import {
+  describe, expect, jest, it,
+} from '@jest/globals';
 import Editora from '../../models/editora';
 
 describe('Teste do model Enditade', () => {
@@ -17,11 +19,35 @@ describe('Teste do model Enditade', () => {
     );
   });
 
-  it('Deve salvar editora no banco de dados', async () => {
+  it.skip('Deve salvar editora no banco de dados', async () => {
     const editora = new Editora(objectoEditora);
 
     const dados = await editora.salvar();
     const retornado = await Editora.pegarPeloId(dados.id);
+
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objectoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
+    );
+  });
+
+  it('Deve salvar editora no banco de dados', async () => {
+    const editora = new Editora(objectoEditora);
+
+    editora.salvar = jest.fn().mockReturnValue({
+      id: 0,
+      nome: 'FJ',
+      cidade: 'Luanda',
+      email: 'fjeditora@gmail.com',
+      created_at: '2023-11-03',
+      updated_at: '2023-11-03',
+    });
+
+    const retornado = editora.salvar();
 
     expect(retornado).toEqual(
       expect.objectContaining({
